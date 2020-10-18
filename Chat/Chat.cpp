df63b3bca0a78591a4e636ef92ff1b127dad8e5d
Karabas1 <nikitarafa2232@gmail.com>
2020-10-18 16:58:16 +0300

    Initial commit

---
+++ Chat/Chat.cpp
@@ -0,0 +1,32 @@
+﻿#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <Windows.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_DEPRECATE
+#pragma warning(disable : 4996)
+
+int main()
+{
+
+		FILE* file;
+		char buffer[128];
+
+		file = fopen("C:/d/test.txt", "w");
+		printf("Last error: %d", GetLastError());
+		fprintf(file, "Hello, World!");
+		fclose(file);
+		file = fopen("C:/d/test.txt", "r");
+		fgets(buffer, 127, file);
+		printf("%s", buffer);
+		fclose(file);
+		_getch();
+}
+//struct ClientConfiguration;
+	//{
+	//	char* server_ip;
+	//	char* username;
+	//	int password;
+	//	double rating;
+	//};
